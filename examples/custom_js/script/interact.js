b38148f3d4eb4f93b464c43992687e1717a2f631
DPeterK <peter.killick@informaticslab.co.uk>
2021-07-30 14:42:05 +0100

    Improve UX

--- examples/custom_js/script/interact.js
+++ examples/custom_js/script/interact.js
@@ -1,9 +1,24 @@
 $( document ).ready( function() {
+    // Display initial values for sliders.
+    let inputs = [ "n", "m", "rn", "rm" ]
+    for ( var i=0;i<=inputs.length;i++ ) {
+        nameRange = '#' + inputs[i] + 'Range';
+        nameValue = '#' + inputs[i] + 'Value';
+        $( nameValue ).html( $( nameRange ).val() );
+    }
+
+    // Update displayed values for sliders on slider changes.
+    $( '#nRange' ).change(function () { $( '#nValue' ).html( $( '#nRange' ).val() ) });
+    $( '#mRange' ).change(function () { $( '#mValue' ).html( $( '#mRange' ).val() ) });
+    $( '#rnRange' ).change(function () { $( '#rnValue' ).html( $( '#rnRange' ).val() ) });
+    $( '#rmRange' ).change(function () { $( '#rmValue' ).html( $( '#rmRange' ).val() ) });
+
+    // Submit values of sliders to plot point calculator on button click.
     $( "#trigger" ).click( function ( event ) {
-        let n = $( '#nValue' ).val().toString();
-        let m = $( '#mValue' ).val().toString();
-        let rn = $( '#rnValue' ).val().toString();
-        let rm = $( '#rmValue' ).val().toString();
+        let n = $( '#nRange' ).val().toString();
+        let m = $( '#mRange' ).val().toString();
+        let rn = $( '#rnRange' ).val().toString();
+        let rm = $( '#rmRange' ).val().toString();
         console.log("n: " + n + ", m: " + m + ", rn: " + rn + ", rm: " + rm);
 
         let url = "http://localhost:8000/plot";
