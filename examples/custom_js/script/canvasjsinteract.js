32989fa0572a8040ba9ca76f21ff8c2b26111940
Peter Killick <peter.killick@informaticslab.co.uk>
2021-07-30 16:11:21 +0100

    Plotting improvements (#3)
    * Improve UX
    
    * Add canvasjs example
    
    * Remove spurious code

--- examples/custom_js/script/canvasjsinteract.js
+++ examples/custom_js/script/canvasjsinteract.js
@@ -24,44 +24,41 @@ $( document ).ready( function() {
         let url = "http://localhost:8000/plot";
         let data = '{ "n": ' + n + ', "m": ' + m + ', "rn": ' + rn + ', "rm": ' + rm + ' }';
         $.post( url, data, function ( data ) {
-            draw( "#spirograph", data.x, data.y );
+            drawChart( data.x, data.y );
         }, "json" );
     })
 });
 
-function draw(elementName, x, y) {
-    // set up canvas
-    const canvas = document.querySelector( elementName );
 
-    if (!canvas.getContext) {
-        console.log("No canvas found!")
-        return;
+function drawChart(xVals, yVals) {
+    let data = [];
+    for ( var i=0;i<=xVals.length;i++ ) {
+        data.push({
+            x: xVals[i],
+            y: yVals[i]
+        });
     }
-    const ctx = canvas.getContext('2d');
-
-    // Clear the canvas ahead of drawing
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // set line stroke and line width
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = 1;
-
-    // set starting point at centre of canvas
-    let xCentre = 0.5 + 0.5*canvas.width;
-    let yCentre = 0.5 + 0.5*canvas.height;
-    let scale = 0.05*canvas.width;
-
-    // draw a line
-    ctx.beginPath();
-    for ( var i=0; i<=x.length; i++ ) {
-        if ( i===0 ) {
-            // move to the first point
-            ctx.moveTo( ( x[i]*scale )+xCentre, ( y[i]*scale )+yCentre );
-        } else {
-            // now draw lines to all successive points
-            ctx.lineTo( ( x[i]*scale )+xCentre, ( y[i]*scale )+yCentre );
-        }
-    }
-
-    ctx.stroke();
+    let options = {
+        animationEnabled: false,
+        axisX: {
+            minimum: -10,
+            maximum: 10,
+            gridThickness: 0,
+            valueFormatString: " ",
+            tickLength: 0
+        },
+        axisY: {
+            minimum: -10,
+            maximum: 10,
+            gridThickness: 0,
+            valueFormatString: " ",
+            tickLength: 0
+        },
+        data: [{
+            type: "line",
+            markerSize: 0,
+            dataPoints: data,
+        }]
+    };
+    $( "#spirograph" ).CanvasJSChart(options);
 }
